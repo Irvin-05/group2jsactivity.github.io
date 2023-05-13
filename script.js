eb7e48d0da1be6950352c363f078ea2d22563f6e
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-05-13 13:48:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const apiKey = "c731377b7a6bc605c68e6b846819eab1";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metrics&q=manila";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
- async function checkWeather(){
-  const response = await fetch(apiUrl +`&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+ async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").  innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
  }
 
- checkWeather();
+ searchBtn.addEventListener("click", ()=>{
+  checkWeather(searchBox.value);
+ })
+
+ checkWeather("manila"); 
+
