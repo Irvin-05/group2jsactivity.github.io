700c2e141d2bbb8085debb404768eac61c272a28
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-05-13 11:52:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
-const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+const apiKey = "c731377b7a6bc605c68e6b846819eab1";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metrics&q=manila";
 
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
-
-const dispalyResult = (lon, lat, data) => {
+ async function checkWeather(){
+  const response = await fetch(apiUrl +`&appid=${apiKey}`);
+  var data = await response.json();
 
   console.log(data);
-  let element = document.createElement("h1");
-  element.innerText = `Weather result in ${lon}, ${lat} is: ${data.temperature}`;
-  body.append(element);
-}
+ }
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-function success(pos) {
-  const crd = pos.coords;
-
-
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${crd.latitude}&longitude=${crd.longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
-    .then(result => result.json()).then(
-      data => {
-        dispalyResult(crd.latitude, crd.longitude, data.current_weather);
-      }
-    );
-
-
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
+ checkWeather();
